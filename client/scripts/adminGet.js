c089cd1afd64cc63f9c9e4902f5671ddc1bb674a
Islam Khabibullin <islam.khabibulin@mera.com>
2022-03-13 11:17:37 +0300

    1.0

--- client/scripts/adminGet.js
+++ client/scripts/adminGet.js
@@ -9,30 +9,41 @@ const status = function (response) {
 const json = function (response) {
   return response.json();
 }
-let vacancy = fetch('http://127.0.0.1:8080/api/admin/vacancy' , {
-// let vacancy = fetch('https://run.mocky.io/v3/93215ae8-0ab1-477f-a91c-5011a5bca0f7' , {
-  method: 'get',
-  headers: {
-    'Key': 'secret'
-  },
-})
+// let vacancy = fetch('http://127.0.0.1:8080/api/admin/vacancy' , {
+let vacancy = fetch(
+    'http://127.0.0.1:8080/api/admin/vacancy', {
+      method: 'get'
+    })
 .then(status)
 .then(json)
 .catch(function (error) {
   console.log('error', error)
 })
 
-vacancy.then( function (result) {
+vacancy.then(function (result) {
   result.forEach((vacancy) => {
     const option = document.createElement('option');
     option.value = vacancy.id;
     option.textContent = vacancy.name;
-    selectList[0].appendChild(option);
-    selectList[1].appendChild(option);
     selectList.forEach((list) => {
-      const opt =  option.cloneNode(true)
-      list.appendChild(opt);
-    })
+      const opt = option.cloneNode(true);
+      list.appendChild(opt)
+    });
+    const vacTable = document.querySelector('.table2');
+    const innerTable = vacTable.querySelector('tbody');
+    const row = document.createElement('tr');
+    const vacancyName = document.createElement('td');
+    const vacancyInfo = document.createElement('td');
+    let questionsRow = '<td>';
+    vacancy.questions.forEach((question) => {
+      questionsRow += question.content + ' <br> ';
+    });
+    vacancyName.textContent = vacancy.name;
+    vacancyInfo.textContent = questionsRow;
+    row.appendChild(vacancyName);
+    row.innerHTML += questionsRow + '</td>';
+    innerTable.appendChild(row);
+
   });
 });
 //ADD USER
@@ -40,13 +51,16 @@ const addUserButton = document.querySelector('.add-user-button');
 addUserButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   const userName = document.querySelector('input[name=userName]').value;
-  const vacancyId = document.querySelector('.selector').value;
-  fetch('http://localhost:8080/api/admin/user', {
+  const vacancyId = document.querySelector('.selector1').value;
+  fetch('http://127.0.0.1:8080/api/admin/user', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({username: userName, vid: vacancyId})
+    body: JSON.stringify({
+      username: userName,
+      vid: vacancyId
+    })
   })
 })
 
@@ -55,7 +69,7 @@ const addVacancyButton = document.querySelector('.add-vacancy-button');
 addVacancyButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   const vacancyName = document.querySelector('input[name=vacancyName]').value;
-  console.log(vacancyName)
+  const vacJson = JSON.stringify(vacancyName);
   fetch('http://127.0.0.1:8080/api/admin/vacancy', {
     method: 'post',
     headers: {
@@ -70,12 +84,61 @@ const addQuestionButton = document.querySelector('.add-question-button');
 addQuestionButton.addEventListener('click', (evt) => {
   evt.preventDefault();
   const questionText = document.querySelector('input[name=questionText]').value;
-  const vacancyId = document.querySelector('.selector').value;
+  const vacancyId = document.querySelector('.selector2').value;
   fetch('http://127.0.0.1:8080/api/admin/vacancy/' + vacancyId + '/question', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({content: questionText})
+    body: JSON.stringify({
+      content: questionText
+    })
   })
 })
+
+////TABLE ULTRA
+
+let videoTable = fetch(
+    'http://127.0.0.1:8080/api/admin/user', {
+      method: 'get'
+    })
+.then(status)
+.then(json)
+.catch(function (error) {
+  console.log('error', error)
+})
+
+videoTable.then(function (result) {
+  const tableBody = document.querySelector('.table1 tbody');
+  const tableR = document.createElement('tr');
+  console.log(result)
+  result.forEach((row) => {
+    const userId = row.id;
+    const userName = row.username;
+    const url = row.url;
+    const vacancy = row.vacancy;
+
+    const tableRow = tableR.cloneNode(false);
+    const userIdTd = document.createElement('td');
+    userIdTd.textContent = userId;
+    const userNameTd = document.createElement('td');
+    userNameTd.textContent = userName;
+    const vacancyTd = document.createElement('td');
+    vacancyTd.textContent = vacancy;
+    const urlTd = document.createElement('td');
+    if (url === null) {
+      urlTd.textContent = 'null';
+    } else {
+      const videoLink = document.createElement('a');
+      videoLink.href = url;
+      videoLink.textContent = 'Показать собеседование';
+      urlTd.appendChild(videoLink);
+    }
+
+    tableRow.appendChild(userIdTd);
+    tableRow.appendChild(userNameTd);
+    tableRow.appendChild(vacancyTd);
+    tableRow.appendChild(urlTd);
+    tableBody.appendChild(tableRow);
+  })
+});
